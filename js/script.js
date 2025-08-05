4ed4fe7ed43505d53b85a2d8fd2c6520f6e008a9
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-08-06 00:26:39 +0600

    constractor fun add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-
 // ---------fisrt step constractor er data push method a neyar jonno array creat 
 const addConstarctorData = [];
 
-// -----------sceond step constructor functions 
 
+// -----------sceond step constructor functions 
+function studentINfo(name, roll, id, clas, age, shift) {
+
+  this.name = name;
+  this.roll = roll;
+  this.id = id;
+  this.class = clas;
+  this.age = age;
+  this.sfift = shift;
+
+  this.UserdataCatch = function () {
+
+    return `         <div class="showStudent">
+              <p>Studdent name: ${this.name}</p>
+              <p>Roll: ${this.roll}</p>
+              <p>I'D: ${this.roll}</p>
+              <p>Class: ${this.clas} </p>
+              <p>Age: ${this.age}</p>
+              <p>Shift: ${this.shift}</p>
+          </div>`;
+
+  }
+
+}
